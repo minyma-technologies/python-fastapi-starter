036855a8774e8f1db95b0fd6f34bdd3c563085d3
godraadam <godra.adam@gmail.com>
2023-02-05 14:07:30 +0200

    fix(repo): datetime serialization bug fixed

--- app/repo/base.py
+++ app/repo/base.py
@@ -22,8 +22,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         return db.query(self.model).offset(skip).limit(limit).all()
 
     def create(self, db: Session, obj_in: CreateSchemaType) -> ModelType:
-        obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data)  # type: ignore
+        db_obj = self.model(**obj_in.dict())  # type: ignore
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
